b391edabe905c7990caad31385c8c5c52edfd644
Kundan <kundanjadhav2001@gmail.com>
2022-10-09 16:03:10 +0530

    minor changes

--- loginvalidation.php
+++ loginvalidation.php
@@ -14,7 +14,7 @@ session_start();
             $err = "please enter the values...";
         }
     if(empty($err)){
-        $sql = "select id, username, password from eventmanagement where username = ?";
+        $sql = "select username, password from logininfo where username = ?";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "s", $param_username);
         $param_username = $username;
@@ -22,7 +22,7 @@ session_start();
         if(mysqli_stmt_execute($stmt)){
             mysqli_stmt_store_result($stmt);
             if(mysqli_stmt_num_rows($stmt) == 1){
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // this means the password is corrct. Allow user to login
@@ -32,7 +32,7 @@ session_start();
                             $_SESSION["loggedin"] = true;
     
                             //Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: index.php");
                                 
                         }
                     }
